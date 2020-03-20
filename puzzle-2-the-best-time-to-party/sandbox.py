0c8e1b542ddf551e60ab986864bc180ca64722f9
Susensio <susensio@desktop.home>
2020-03-20 23:55:28 +0100

    Add puzzle 2 sandbox

---
+++ puzzle-2-the-best-time-to-party/sandbox.py
@@ -0,0 +1,51 @@
+# Just playing around while watching the lecture, before seeing the code
+
+data = [
+    ('Beyoncé', 6, 7),
+    ('Taylor', 7, 9),
+    ('Brad', 10, 11),
+    ('Katy', 10, 12),
+    ('Tom', 8, 10),
+    ('Drake', 9, 11),
+    ('Alicia', 6, 8),
+]
+
+
+def best_hour(schedule):
+    hours = []
+    for person, comes, goes in schedule:
+        for h in range(comes, goes):
+            hours.append(h)
+    return more_frequent(hours)
+
+
+def more_frequent(array):
+    # Initialize counter
+    counter = {el: 0 for el in set(array)}
+    # Do de counting
+    for el in array:
+        counter[el] += 1
+
+    # Get key with largest value
+    return max(counter, key=counter.get)
+
+
+result = best_hour(data)
+print(result)
+assert result == 10
+
+
+# Incremental algorithm
+def best_hour_inc(schedule):
+    movements = []
+    for person, comes, goes in schedule:
+        movements.append((comes, 1))
+        movements.append((goes, -1))
+    movements.sort()
+    density = {}
+    for hour, change in movements:
+        density[hour] = density.get(hour, density.get(hour-1, 0)) + change
+    return max(density, key=density.get)
+
+
+print(best_hour_inc(data))
