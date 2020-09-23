ed26aa7275104b6ff8184218a7cce1cb422d5acc
Susensio <susensio@desktop>
2020-09-23 08:46:29 +0200

    Add P5E3

--- puzzle-5-keep-those-queens-apart/4queens-matrix.py
+++ puzzle-5-keep-those-queens-apart/4queens-matrix.py
@@ -54,17 +54,24 @@ def FourQueens(n=4):
         board[i][0] = 1
         for j in range(n):
             board[j][1] = 1
-            if noConflicts(board, 1, j, n):
-                for k in range(n):
-                    board[k][2] = 1
-                    if noConflicts(board, 2, k, n):
-                        for m in range(n):
-                            board[m][3] = 1
-                            if noConflicts(board, 3, m, n):
-                                print(board)
-                            board[m][3] = 0
+            if not noConflicts(board, 1, j, n):
+                board[j][1] = 0
+                continue
+            for k in range(n):
+                board[k][2] = 1
+                if not noConflicts(board, 2, k, n):
                     board[k][2] = 0
+                    continue
+                for m in range(n):
+                    board[m][3] = 1
+                    if noConflicts(board, 3, m, n):
+                        print(board)
+                    board[m][3] = 0
+
+                board[k][2] = 0
+
             board[j][1] = 0
+
         board[i][0] = 0
     return
 
