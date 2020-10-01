0d5ce80a06c7790ab4d147ec779fd1647bf056ce
Susensio <susensio@desktop>
2020-10-01 09:07:56 +0200

    [WIP] Continue with P7E2, center problem

--- puzzle-7-tile-that-courtyard-please/binary_search_matrix.py
+++ puzzle-7-tile-that-courtyard-please/binary_search_matrix.py
@@ -29,18 +29,22 @@ def binary_search_2d(matrix, value):
             return middle(matrix)
         elif center(matrix) < value:
             # Remove 2nd quadrant (index 1)
-            quadrants.pop(1)
+            quadrants.pop(2)
         else:
             # Only keep 2nd quadrant
-            quadrants = [quadrants[1]]
+            quadrants = {2: quadrants[2]}
 
-        for q in quadrants:
-            found = binary_search_2d(q, value)
+        for i, quad in quadrants.items():
+            found = binary_search_2d(quad, value)
             if found:
                 row, col = found
-                # BUG
-                # Aqui no estoy sumando bien...
-                return row+len(q), col+len(q[0])
+                mid_row, mid_col = middle(matrix)
+                if i in (1, 4):
+                    col = col + mid_col + 1
+                if i in (3, 4):
+                    row = row + mid_row + 1
+
+                return row, col
 
 
 def center(matrix):
@@ -49,10 +53,67 @@ def center(matrix):
 
 
 def middle(matrix):
+    # BUG
+    # Aqui el problema es que no soy consitente en la elección del centro
+    # Si quiero que el centro sea parte del segundo cuadrante de split_into_quadrants, tengo que darle una vuelta
     middle_row = len(matrix) // 2
     middle_col = len(matrix[0]) // 2
     return middle_row, middle_col
 
 
+def test_binary_search_2d_first_quad():
+    assert binary_search_2d(T, 11) == (0, 3)
+
+
+def test_binary_search_2d_second_quad():
+    assert binary_search_2d(T, 1) == (0, 0)
+
+
+def test_binary_search_2d_third_quad():
+    assert binary_search_2d(T, 21) == (4, 1)
+
+
+def test_binary_search_2d_fourth_quad():
+    assert binary_search_2d(T, 30) == (4, 4)
+
+
+def test_binary_search_2d_not_found():
+    assert binary_search_2d(T, 100) is None
+
+
+def test_binary_search_2d_square_center():
+    assert binary_search_2d(T, 9) == (2, 2)
+
+
+def test_binary_search_2d_square_first_quad():
+    assert binary_search_2d(T_sq, 11) == (0, 3)
+
+
+def test_binary_search_2d_square_second_quad():
+    assert binary_search_2d(T_sq, 1) == (0, 0)
+
+
+def test_binary_search_2d_square_third_quad():
+    assert binary_search_2d(T_sq, 21) == (4, 1)
+
+
+def test_binary_search_2d_square_fourth_quad():
+    assert binary_search_2d(T_sq, 30) == (4, 4)
+
+
+def test_binary_search_2d_square_not_found():
+    assert binary_search_2d(T_sq, 1000) is None
+
+
 if __name__ == "__main__":
-    print(binary_search_2d(T, 21))
+    search = 21
+    print(f"{search} is in {binary_search_2d(T, search)}")
+
+    search = 17
+    print(f"{search} is in {binary_search_2d(T, search)}")
+
+    search = 1
+    print(f"{search} is in {binary_search_2d(T, search)}")
+
+    search = 35
+    print(f"{search} is in {binary_search_2d(T, search)}")
