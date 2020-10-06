b50f1251c788e321f5543d7ff26aa7cf9a6afbed
Susensio <susensio@desktop>
2020-10-06 08:25:34 +0200

    Add almost brute force solver with pruning

--- puzzle8-you-wont-want-to-play-sudoku-again/sandbox8.py
+++ puzzle8-you-wont-want-to-play-sudoku-again/sandbox8.py
@@ -1,4 +1,49 @@
 
+def solve_sudoku(board):
+    # Find an empty cell
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == -1:
+                # Try to place a number
+                for number in range(1, 10):
+                    new_board, allowed = place_sudoku_cell(
+                        board, row, col, number)
+                    if allowed:
+                        # Branch down
+                        attemp = solve_sudoku(new_board)
+                        if attemp:
+                            return attemp
+
+                # When no number is valid, prune tree and backtrack
+                return
+
+    # All cells are filled
+    return board
+
+
+def place_sudoku_cell(board, row, col, number):
+    new_board = [row.copy() for row in board]
+    new_board[row][col] = number
+
+    horizontal_unique = not (number in board[row])
+    vertical_unique = not (number in [row[col] for row in board])
+
+    box_row = row//3
+    box_row_start = box_row*3
+    box_row_end = (box_row+1)*3
+    box_col = col//3
+    box_col_start = box_col*3
+    box_col_end = (box_col+1)*3
+    box = [cell
+           for row in board[box_row_start:box_row_end]
+           for cell in row[box_col_start:box_col_end]]
+    box_unique = not (number in box)
+
+    allowed = horizontal_unique and vertical_unique and box_unique
+
+    return new_board, allowed
+
+
 def pretty_print_sudoku(board):
     HEAVY_UPPER = "┏━━━┯━━━┯━━━┳━━━┯━━━┯━━━┳━━━┯━━━┯━━━┓"
     HEAVY_MIDDLE = "┣━━━┿━━━┿━━━╋━━━┿━━━┿━━━╋━━━┿━━━┿━━━┫"
@@ -47,3 +92,4 @@ if __name__ == "__main__":
         [-1, -1, -1, 8, -1, 5, -1, -1, -1]
     ]
     print(pretty_print_sudoku(board))
+    print(pretty_print_sudoku(solve_sudoku(board)))
