e6c8d732e73bfa6e17650e8a33491650bb79d81d
Susensio <susensio@desktop>
2020-11-19 19:00:05 +0100

    Add exercise 1

--- puzzle8-you-wont-want-to-play-sudoku-again/sudoku-opt.py
+++ puzzle8-you-wont-want-to-play-sudoku-again/sudoku-opt.py
@@ -48,44 +48,50 @@ def makeImplications(grid, i, j, e):
     grid[i][j] = e
     impl = [(i, j, e)]
 
-    for k in range(len(sectors)):
+    while True:
+        previous_implication_counter = len(impl)
+        for k in range(len(sectors)):
 
-        sectinfo = []
+            sectinfo = []
 
-        # find missing elements in ith sector
-        vset = {1, 2, 3, 4, 5, 6, 7, 8, 9}
-        for x in range(sectors[k][0], sectors[k][1]):
-            for y in range(sectors[k][2], sectors[k][3]):
-                if grid[x][y] != 0:
-                    vset.remove(grid[x][y])
+            # find missing elements in ith sector
+            vset = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+            for x in range(sectors[k][0], sectors[k][1]):
+                for y in range(sectors[k][2], sectors[k][3]):
+                    if grid[x][y] != 0:
+                        vset.remove(grid[x][y])
 
-        # attach copy of vset to each missing square in ith sector
-        for x in range(sectors[k][0], sectors[k][1]):
-            for y in range(sectors[k][2], sectors[k][3]):
-                if grid[x][y] == 0:
-                    sectinfo.append([x, y, vset.copy()])
+            # attach copy of vset to each missing square in ith sector
+            for x in range(sectors[k][0], sectors[k][1]):
+                for y in range(sectors[k][2], sectors[k][3]):
+                    if grid[x][y] == 0:
+                        sectinfo.append([x, y, vset.copy()])
 
-        for m in range(len(sectinfo)):
-            sin = sectinfo[m]
+            for m in range(len(sectinfo)):
+                sin = sectinfo[m]
 
-            # find the set of elements on the row corresponding to m and remove them
-            rowv = set()
-            for y in range(9):
-                rowv.add(grid[sin[0]][y])
-            left = sin[2].difference(rowv)
+                # find the set of elements on the row corresponding to m and remove them
+                rowv = set()
+                for y in range(9):
+                    rowv.add(grid[sin[0]][y])
+                left = sin[2].difference(rowv)
 
-            # find the set of elements on the column corresponding to m and remove them
-            colv = set()
-            for x in range(9):
-                colv.add(grid[x][sin[1]])
-            left = left.difference(colv)
+                # find the set of elements on the column corresponding to m and remove them
+                colv = set()
+                for x in range(9):
+                    colv.add(grid[x][sin[1]])
+                left = left.difference(colv)
 
-            # check if the vset is a singleton
-            if len(left) == 1:
-                val = left.pop()
-                if isValid(grid, sin[0], sin[1], val):
-                    grid[sin[0]][sin[1]] = val
-                    impl.append((sin[0], sin[1], val))
+                # check if the vset is a singleton
+                if len(left) == 1:
+                    val = left.pop()
+                    if isValid(grid, sin[0], sin[1], val):
+                        grid[sin[0]][sin[1]] = val
+                        impl.append((sin[0], sin[1], val))
+
+        if previous_implication_counter == len(impl):
+            # Nothing changed
+            break
 
     return impl
 
