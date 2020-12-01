87c95c5cf80d0a199826ac6bb5621e4a0c63d17b
Susensio <snogueras@daymsa.com>
2020-12-01 20:12:04 +0100

    Solve exercise 1

--- puzzle-9-the-disorganized-handyman/quicksort-inplace.py
+++ puzzle-9-the-disorganized-handyman/quicksort-inplace.py
@@ -9,6 +9,7 @@
 # This procedure selects a pivot and partitions the list into 3 sublists
 # It only uses one element worth of additional storage for the pivot!
 def pivotPartitionClever(lst, start, end):
+    moves = 0
     pivot = lst[end]
     bottom = start - 1
     top = end
@@ -23,6 +24,7 @@ def pivotPartitionClever(lst, start, end):
                 done = True
                 break
             if lst[bottom] > pivot:
+                moves += 1
                 lst[top] = lst[bottom]
                 # print (lst, 'bottom =', bottom, 'top = ', top)
                 break
@@ -34,29 +36,39 @@ def pivotPartitionClever(lst, start, end):
                 done = True
                 break
             if lst[top] < pivot:
+                moves += 1
                 lst[bottom] = lst[top]
                 # print (lst, 'bottom =', bottom, 'top = ', top)
                 break
 
     lst[top] = pivot
     # print (lst)
-    return top
+    return top, moves
 
 
-def quicksort(lst, start, end):
+def quicksort(lst, start=0, end=None):
+    moves = 0
+    if end is None:
+        end = len(a) - 1
     if start < end:
         # print ('Partition start: bottom =', start - 1, 'top = ', end)
         # print (lst)
-        split = pivotPartitionClever(lst, start, end)
+        split, moves = pivotPartitionClever(lst, start, end)
         # print ('Partition end')
-        quicksort(lst, start, split - 1)
-        quicksort(lst, split + 1, end)
-    return
+        moves += quicksort(lst, start, split - 1)
+        moves += quicksort(lst, split + 1, end)
+    return moves
 
 
-a = [4, 65, 2, -31, 0, 99, 83, 782, 1]
-print('Initial list is:', a)
-quicksort(a, 0, len(a) - 1)
-print('Sorted list is:', a)
+if __name__ == "__main__":
+    a = [4, 65, 2, -31, 0, 99, 83, 782, 1]
+    print('Initial list is:', a)
+    moves = quicksort(a)
+    print('Sorted list is:', a)
+    assert moves == 9
 
-b = [4, 4, 65, 2, -31, 0, 99, 83, -31, 782, 1]
+    already_sorted = list(range(100))
+    moves = quicksort(already_sorted)
+    assert moves == 0
+
+    b = [4, 4, 65, 2, -31, 0, 99, 83, -31, 782, 1]
